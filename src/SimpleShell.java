4b65159d29e4a5add84a358e3381f75aca0b6052
Alicia McNett <amcnettpct@gmail.com>
2018-09-05 14:01:53 -0400

    Created initial code for assignment

---
+++ src/SimpleShell.java
@@ -0,0 +1,26 @@
+// This program starts Assignment01 for you by laying the foundation for the 
+// simple shell program.
+// Date: 9/5/18
+// Author: McNett
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class SimpleShell{
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+		// defines template for our process -- specifies process attributes 
+		//ProcessBuilder pb = new ProcessBuilder("ls"); //linux command
+		ProcessBuilder pb = new ProcessBuilder("bash", "-c", "ls"); //windows 10 command
+		
+		Process p = pb.start();// starts process
+		
+		p.waitFor(); //to ensure output is ready
+		
+		// get process output as our input and display to screen
+		Scanner input = new Scanner(p.getInputStream());
+		while(input.hasNext())
+			System.out.println(input.next()); 
+		
+	}
+}
